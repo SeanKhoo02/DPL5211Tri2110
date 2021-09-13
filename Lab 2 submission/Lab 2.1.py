61852f2b969aa55b5c86e52264d47ddd2c68458a
SeanKhoo02 <88395186+SeanKhoo02@users.noreply.github.com>
2021-09-13 10:48:14 +0800

    Delete Lab 2.1.py

--- Lab 2 submission/Lab 2.1.py
+++
@@ -1,9 +0,0 @@
-# Student ID: 1201201336
-# Student Name: Khoo Chong Qin
-
-kilo = float(input("Please enter the value of kilogram : "))
-gram = kilo * 1000
-print("For ", kilo, " kilogram is equivaldent to ", round(gram, 2), " gram")
-
-#other method
-print("For {:.2f} kilogram is equivalent to {:.2f} gram".format(kilo, gram))
