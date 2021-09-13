e86fb9767b639735f6474103475dfe12ecbfb79f
SeanKhoo02 <88395186+SeanKhoo02@users.noreply.github.com>
2021-09-13 10:52:16 +0800

    Update Lab 2.5.py

--- Lab 2 submission/Lab 2.5.py
+++ Lab 2 submission/Lab 2.5.py
@@ -1,12 +1,12 @@
-const = 0.15
+W = 0.15
 
 print("Natural Mineral Water Dispenser")
 print("-------------------------------------")
 liters = int(input("\nEnter amount of liters: "))
 
-payment=liters*const
+payment=liters*W
 
-print("\nPrice per litre   = RM ", const)
+print("\nPrice per litre   = RM ", W)
 print("Number of liters = ", liters)
 print("Total: RM {:.2f} ".format(payment))
 
